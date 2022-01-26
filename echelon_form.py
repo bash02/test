7c5a4cbcb7521c8090a0fd055579dcf39ff4b723
bash02 <69587323+bash02@users.noreply.github.com>
2022-01-26 12:11:56 +0100

    Update echelon_form.py

--- echelon_form.py
+++ echelon_form.py
@@ -1,14 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import sympy as sp
-
-
-# In[2]:
-
 
 a = sp.Matrix([
     [1, 2, -3, 0],
@@ -16,15 +6,4 @@ a = sp.Matrix([
     [3, 6, -4, 3]
 ])
 
-
-# In[3]:
-
-
 a.echelon_form()
-
-
-# In[ ]:
-
-
-
-
